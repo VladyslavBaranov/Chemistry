5ed3df76f59d906cb11e83bc46ddcd22b5c6ef9c
VladyslavBaranov <Apple020801>
2022-04-03 13:27:51 +0300

    Initial Commit

---
+++ ElementsSystemUITests/ElementsSystemUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ElementsSystemUITestsLaunchTests.swift
+//  ElementsSystemUITests
+//
+//  Created by Vladyslav Baranov on 03.04.2022.
+//
+
+import XCTest
+
+class ElementsSystemUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
